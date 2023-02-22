fce41abac7573a2ce51b1c62836abe257820d4f5
Arman <mesropyanarman4@gmail.com>
2023-02-22 12:08:35 +0400

    files upload

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss'
+
+const Contact = () => {
+    return (
+        <div className='P-contact G-container'>
+            <p className='P-burning-end'>THE END</p>
+        </div>
+    );
+};
+
+export default Contact;
---
+++ src/router/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Route} from "react-router-dom";
+import Home from "../pages/home";
+import Trip from "../pages/trip";
+import Preloader from "../utils/preloader";
+import usePreloader from "../hook/usePreloader";
+import About from "../pages/about";
+import Contact from "../pages/contact";
+
+const RouterComponent = () => {
+    const {loading} = usePreloader()
+    if (loading) {
+        return <Preloader/>
+    }
+    return (
+        <>
+            <Route exact path='/' render={() => <Home/>}/>
+            <Route exact path='/trip' render={() => <Trip/>}/>
+            <Route exact path='/about' render={() => <About/>}/>
+            <Route exact path='/end' render={() => <Contact/>}/>
+        </>
+    );
+};
+
+export default RouterComponent;
